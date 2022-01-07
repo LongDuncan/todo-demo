c6080af31362d6a3485396c6b2f6349e70c4c75c
Chang-Yu Chiang <long0709@gmail.com>
2022-01-07 11:03:45 +0800

    feat: Add unit test functions 1. testUpdate 2. testDelete

--- src/test/kotlin/com/demo/todo/ToDoControllerTest.kt
+++ src/test/kotlin/com/demo/todo/ToDoControllerTest.kt
@@ -15,7 +15,8 @@ import org.junit.jupiter.api.TestMethodOrder
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
-
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import io.restassured.RestAssured.given
 import org.hamcrest.CoreMatchers.`is`
 import org.json.JSONObject
@@ -23,6 +24,7 @@ import com.google.common.base.Objects
 
 @QuarkusTest
 @TestTransaction
+@TestInstance(PER_CLASS)
 @QuarkusTestResource(H2DatabaseTestResource::class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 class ToDoControllerTest {
@@ -67,11 +69,46 @@ class ToDoControllerTest {
             .get("/api/1")
         .then()
             .statusCode(200)
-            .body("completed", `is`(true))
+            .body("completed", `is`(false))
             .body("order", `is`(0))
-            .body("title", `is`("Introduction to Quarkus"))
+            .body("title", `is`("Test ToDo One"))
             .body("url", `is`("string"))
     }
 
-    val ONE:String = "{\"completed\":true,\"order\":0,\"title\":\"Introduction to Quarkus\",\"url\": \"string\"}"
+    @Test @Order(5)
+    fun testUpdate() {
+        given()
+            .contentType(ContentType.JSON)
+        .`when`()
+            .body(TWO)
+            .patch("/api/1")
+        .then()
+            .statusCode(200)
+            .body("completed", `is`(true))
+            .body("order", `is`(0))
+            .body("title", `is`("Test ToDo Two"))
+            .body("url", `is`("string"))
+    }
+
+    @ParameterizedTest
+    @Order(6)
+    @MethodSource("todoItemsToDelete")
+    fun testDelete(id:Int, expectedStatus:Int) {
+        given()
+                .pathParam("id", id)
+        .`when`()
+                .delete("/api/{id}")
+        .then()
+                .statusCode(expectedStatus);
+    }
+
+    private fun  todoItemsToDelete():Stream<Arguments> {
+        return Stream.of(
+                Arguments.of(1, 204),
+                Arguments.of(2, 404)
+        );
+    }
+
+    val ONE:String = "{\"completed\":false,\"order\":0,\"title\":\"Test ToDo One\",\"url\": \"string\"}"
+    val TWO:String = "{\"completed\":true,\"order\":0,\"title\":\"Test ToDo Two\",\"url\": \"string\"}"
 }
